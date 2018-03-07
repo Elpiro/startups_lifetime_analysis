2c87d1cfea9f9c42e5468dd3ab0b87eb0740d7f1
Elpiro <32934904+Elpiro@users.noreply.github.com>
2018-03-07 11:14:45 +0100

    Currently merging rounds & companies

--- merge_datasets.R
+++ merge_datasets.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 path = "C:/Users/GALLICE/Documents/Perso/Etudes/DSTI/Cours/Survival Analysis/project/Data/crunchbase-october-2013-master/crunchbase-"
 datafile = "rounds.csv"
 
@@ -11,8 +13,10 @@ sum(table_df$Freq > 1)/length(table_df$Freq) #41% of rounds are associated to mo
 rm(table_df)
 
 
+#sort rounds dataset by startup name
 startups$company_name <- factor(startups$company_name)
 startups <- startups[order(startups$company_name),]
+startups$company_name <- sapply(startups$company_name, as.character)
 
 path = "C:/Users/GALLICE/Documents/Perso/Etudes/DSTI/Cours/Survival Analysis/project/Data/crunchbase-october-2013-master/crunchbase-"
 datafile = "companies.csv"
@@ -20,3 +24,8 @@ datafile = "companies.csv"
 companies <- read.csv(paste(path,datafile, sep= ""), 
                    sep = ',',
                    stringsAsFactors = FALSE)
+
+
+names(companies)[2] <- "company_name"
+
+startups <- left_join(startups, companies, by = "company_name")
