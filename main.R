16ec76992cd45038848ab27821d25674b9eab396
Elpiro <32934904+Elpiro@users.noreply.github.com>
2018-03-08 11:57:59 +0100

    Update main.R

--- main.R
+++ main.R
@@ -23,21 +23,24 @@ startups$last_milestone_at <- as.Date(as.character(startups$last_milestone_at, f
 table(startups$country_code)
 
 
-hist(startups$founded_at, breaks = 30)
-
 plot(startups$status ~ log(startups$funding_total_usd), col = unique(startups$status))
 legend("topleft", legend=levels(startups$status), pch=16, col=unique(startups$status))
 
-plot(startups$status ~ startups$category_code, col = unique(startups$status))
-legend("topleft", legend=levels(startups$status), pch=16, col=unique(startups$status))
-
-
-
+hist(na.omit(startups$founded_at), breaks = 30, main="repartition of foundations dates. Some outliers are visible")
+lower_date_limit <- "1990-01-01"
 
 #adding a column duration
 
 start_date <- startups$founded_at
+#imputes missing dates in last_milestone_at
+startups$last_milestone_at[is.na(startups$last_milestone_at)] <- startups$last_funding_at[is.na(startups$last_milestone_at)]
+end_date <- startups$last_milestone_at
+
+if(FALSE){
 end_date <- startups$last_funding_at #or end_date ? <- startups$last_milestone_at 
+                                     #for startups that are acquired, the last_mileston could be better
+                                     # startups[which(startups$status=="acquired"), c("last_funding_at","last_milestone_at")]
+}
 
 current_lifetime <- data.frame(matrix(data = NA,nrow = dim(startups)[1], ncol = 1))
 
@@ -113,11 +116,24 @@ rounds$company_name <- factor(rounds$company_name)
 rounds <- rounds[order(rounds$company_name),]
 rounds$company_name <- sapply(rounds$company_name, as.character)
 
-startups <- startups <- left_join(rounds, startups, by = "company_name")
+startups <- left_join(rounds, startups, by = "company_name")
 rm(rounds)
 
+rows_to_remove <- startups$founded_at < lower_date_limit
+rows_to_remove[is.na(rows_to_remove)] <- FALSE
+
+startups_1 <- startups[!rows_to_remove,]
 #cleaned dataset
 summary(startups)
+
+#todo : impute missing values in funding_total_usd from raised_amount_usd
+
+
+
+
+
+
+
 
 
 #Survival analysis#
@@ -133,9 +149,16 @@ startups_subset <- data.frame(startups_subset, DUMMY)
 rm(DUMMY)
 
 
+startups_subset$funding_round_type <- factor(startups_subset$funding_round_type)
+DUMMY <- startups_subset$funding_round_type
+DUMMY <- model.matrix(~DUMMY)
+startups_subset <- data.frame(startups_subset, DUMMY)
+rm(DUMMY)
 
 
-censoring_date <- 20*365
+library(ggplot2)
+
+censoring_date <- 10*365
 
 graph_area_subset <- startups[c("days_of_existence", "target")]
 closed_ones <- graph_area_subset[graph_area_subset$target == 1,]
@@ -146,19 +169,30 @@ censored_ones <- censored_ones[!is.na(censored_ones),]
 ggplot(data = censored_ones[censored_ones$days_of_existence > censoring_date,], aes(x=days_of_existence)) + geom_histogram() 
 
 
-par(mfrow=c(1,2))
 
 
 startups_subset <- startups_subset[which(startups_subset$days_of_existence<censoring_date),]
+
+
+rm(graph_area_subset)
+rm(closed_ones)
+rm(censored_ones)
 
 #for regression
 X <- as.matrix(startups_subset[,-which(names(startups_subset) %in% c("company_category_code","target","X.Intercept."))])
 Y <- as.matrix(startups_subset[,c("target")])
 
 #for survival model
-surv_dataframe <- startups_subset[,-which(names(startups_subset) %in% c("company_category_code","days_of_existence","target","X.Intercept."))]
+surv_dataframe <- startups_subset[,-which(names(startups_subset) %in% c("company_category_code","days_of_existence","target","X.Intercept.", "X.Intercept..1"))]
 surv_target <- Surv(time = startups_subset$days_of_existence, event = startups_subset$target)
 surv_dataframe <- data.frame(surv_dataframe,surv_target)
 
+
+
+fit.KM <- survfit(surv_target~1, data=surv_dataframe)
+plot(fit.KM)
+
+if(FALSE){
 fit <- coxph(surv_target~., data=surv_dataframe)
 summary(fit)
+}
