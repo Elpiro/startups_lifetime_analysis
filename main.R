697cdf4290dae34458f856a7a4731eea21168805
Elpiro <32934904+Elpiro@users.noreply.github.com>
2018-03-08 15:49:04 +0100

    Update main.R

--- main.R
+++ main.R
@@ -1,3 +1,5 @@
+#-------------import of companies.csv---------------#
+
 path = "C:/Users/GALLICE/Documents/Perso/Etudes/DSTI/Cours/Survival Analysis/project/Data/crunchbase-october-2013-master/crunchbase-"
 
 datafile = "companies.csv"
@@ -20,20 +22,28 @@ startups$first_funding_at <- as.Date(as.character(startups$first_funding_at, for
 startups$last_funding_at <- as.Date(as.character(startups$last_funding_at, format="%Y-%m-%d"))
 startups$last_milestone_at <- as.Date(as.character(startups$last_milestone_at, format="%Y-%m-%d"))
 
+
+names(startups)[2] <- "company_name"
+
 table(startups$country_code)
 
+#________________companies imported______________#
+
+
+
+#------some plotting-----------#
 
 plot(startups$status ~ log(startups$funding_total_usd), col = unique(startups$status))
 legend("topleft", legend=levels(startups$status), pch=16, col=unique(startups$status))
 
-hist(na.omit(startups$founded_at), breaks = 30, main="repartition of foundations dates. Some outliers are visible")
-lower_date_limit <- "1990-01-01"
+#__________________done plotting________________#
 
-#adding a column duration
+
+
+#-----------computes lifetime of startup------------#
 
 start_date <- startups$founded_at
-#imputes missing dates in last_milestone_at
-startups$last_milestone_at[is.na(startups$last_milestone_at)] <- startups$last_funding_at[is.na(startups$last_milestone_at)]
+startups$last_milestone_at[is.na(startups$last_milestone_at)] <- startups$last_funding_at[is.na(startups$last_milestone_at)]#imputes missing dates in last_milestone_at
 end_date <- startups$last_milestone_at
 
 if(FALSE){
@@ -63,9 +73,14 @@ startups <- startups[!is.na(startups$funding_total_usd),]
 
 rm(current_lifetime)
 
+#____________done with lifetime_____________#
 
 
-#Transform status column to alive/closed
+
+
+
+
+#------------Transform status column to alive/closed-----------#
 table(startups$status)
 
 target <- data.frame(matrix(data = NA,nrow = dim(startups)[1], ncol = 1))
@@ -90,12 +105,12 @@ for (i in seq(1, dim(startups)[1])){
 startups <- cbind(startups,target)
 rm(target)
 
-
-names(startups)[2] <- "company_name"
-
+#___________target column created______________#
 
 
-#merging with rounds#
+
+
+#----------handling rounds.csv-----------#
 
 
 library(dplyr)
@@ -116,49 +131,78 @@ rounds$company_name <- factor(rounds$company_name)
 rounds <- rounds[order(rounds$company_name),]
 rounds$company_name <- sapply(rounds$company_name, as.character)
 
-startups <- left_join(rounds, startups, by = "company_name")
-rm(rounds)
+rounds$funding_round_type <- factor(rounds$funding_round_type)
+
+
+#_____________done with rounds____________#
+
+
+
+#----------removes the values prior to the lower_date_limit--------------#
+
+hist(na.omit(startups$founded_at), breaks = 30, main="repartition of foundations dates. Some outliers are visible")
+lower_date_limit <- "1990-01-01"
 
 rows_to_remove <- startups$founded_at < lower_date_limit
-rows_to_remove[is.na(rows_to_remove)] <- FALSE
+rows_to_remove[is.na(rows_to_remove)] <- TRUE
 
-startups_1 <- startups[!rows_to_remove,]
+startups <- startups[!rows_to_remove,]
 #cleaned dataset
 summary(startups)
 
 #todo : impute missing values in funding_total_usd from raised_amount_usd
 
+-
+#______________removes the values prior to the lower_date_limit_____________#
+
+  
+  
+  
+  
 
 
 
 
 
-
-
-
-#Survival analysis#
+#------------Survival analysis-------------#
 
 library(survival)
 
-startups_subset <- startups[c("company_category_code", "funding_rounds","funding_round_type","raised_amount_usd","funded_at","funding_total_usd","days_of_existence","target")]
-startups_subset <- startups_subset[complete.cases(startups_subset),]
+#converting factor funding_rounds from rounds, to columns of count of this factor
 
-DUMMY <- startups_subset$company_category_code
-DUMMY <- model.matrix(~DUMMY)
-startups_subset <- data.frame(startups_subset, DUMMY)
-rm(DUMMY)
+funding_round_type_surv_shaped <- data.frame(matrix(data = 0, nrow=dim(startups)[1], ncol = 18))
+col_names_fund_round_type <- levels(rounds$funding_round_type)
+types_counts <- paste("count_",levels(rounds$funding_round_type), sep="")
+names(funding_round_type_surv_shaped) <- c(col_names_fund_round_type, types_counts)
+
+for (i in 1:dim(funding_round_type_surv_shaped)[1]){
+  name <- startups$company_name[i]
+  funding_rounds <- as.character(rounds$funding_round_type[which(rounds$company_name == name)])
+  if (length(funding_rounds) > 0){
+    for (j in 1:length(funding_rounds)){
+      if (is.numeric(rounds$raised_amount_usd[which(rounds$company_name == name)][j])){
+        #adds value
+        funding_round_type_surv_shaped[i,funding_rounds[j]] <- rounds$raised_amount_usd[which(rounds$company_name == name)][j]
+                                                             + funding_round_type_surv_shaped[i,funding_rounds[j]]
+      }
+      else{
+        funding_round_type_surv_shaped[i,funding_rounds[j]] <- NA
+      }
+      
+      #adds count to correponding type
+      funding_round_type_surv_shaped[i,paste("count_", funding_rounds[j], sep ="")] <- 1 + funding_round_type_surv_shaped[i,paste("count_", funding_rounds[j], sep ="")]
+      
+    }
+  } 
+}
 
 
-startups_subset$funding_round_type <- factor(startups_subset$funding_round_type)
-DUMMY <- startups_subset$funding_round_type
-DUMMY <- model.matrix(~DUMMY)
-startups_subset <- data.frame(startups_subset, DUMMY)
-rm(DUMMY)
 
+#------------Some more plotting, and selection of censoring_date-----------#
 
 library(ggplot2)
 
-censoring_date <- 10*365
+censoring_date <- 3*365
 
 graph_area_subset <- startups[c("days_of_existence", "target")]
 closed_ones <- graph_area_subset[graph_area_subset$target == 1,]
@@ -166,33 +210,84 @@ closed_ones <- closed_ones[!is.na(closed_ones),]
 censored_ones <- graph_area_subset[graph_area_subset$target == 0,]
 censored_ones <- censored_ones[!is.na(censored_ones),]
 
-ggplot(data = censored_ones[censored_ones$days_of_existence > censoring_date,], aes(x=days_of_existence)) + geom_histogram() 
-
-
-
-
-startups_subset <- startups_subset[which(startups_subset$days_of_existence<censoring_date),]
+ggplot(data = censored_ones[censored_ones$days_of_existence < censoring_date,], aes(x=days_of_existence)) + geom_histogram() 
 
 
 rm(graph_area_subset)
 rm(closed_ones)
 rm(censored_ones)
 
-#for regression
-X <- as.matrix(startups_subset[,-which(names(startups_subset) %in% c("company_category_code","target","X.Intercept."))])
-Y <- as.matrix(startups_subset[,c("target")])
+#____________End plotting____________#
 
-#for survival model
-surv_dataframe <- startups_subset[,-which(names(startups_subset) %in% c("company_category_code","days_of_existence","target","X.Intercept.", "X.Intercept..1"))]
-surv_target <- Surv(time = startups_subset$days_of_existence, event = startups_subset$target)
+
+#shaping dataframe for survival analysis
+
+censoring_filter <- function(x){
+  if((x >censoring_date) || (x == 0)){
+    return(TRUE)
+  }
+  else{
+    return(FALSE)
+  }
+}
+
+rows_to_remove <- sapply(startups$days_of_existence, censoring_filter )  #applying the censoring filter
+rows_to_remove[is.na(rows_to_remove)] <- TRUE #also removing rows with NA lifetime
+
+final_rounds_count <- funding_round_type_surv_shaped[!rows_to_remove,]
+temp_startups <- startups[!rows_to_remove,]
+
+
+surv_dataframe <- temp_startups[c("category_code", "funding_total_usd", "funding_rounds")]
+
+
+surv_target <- Surv(time = temp_startups$days_of_existence, event = temp_startups$target)
+surv_dataframe <- data.frame(surv_dataframe, final_rounds_count)
 surv_dataframe <- data.frame(surv_dataframe,surv_target)
+rm(temp_startups)
 
 
-
+#fitting model
 fit.KM <- survfit(surv_target~1, data=surv_dataframe)
 plot(fit.KM)
 
 if(FALSE){
-fit <- coxph(surv_target~., data=surv_dataframe)
-summary(fit)
+  fit <- coxph(surv_target~., data=surv_dataframe)
+  summary(fit)
 }
+
+
+
+#______________Survival analysis_____________#
+
+
+
+#-------------linear model--------------#
+
+
+startups <- left_join(rounds, startups, by = "company_name") #merging startups and rounds to get a linear model shaped dataframe
+rm(rounds) 
+
+startups_subset <- startups[c("company_category_code", "funding_rounds","funding_round_type","raised_amount_usd","funded_at","funding_total_usd","days_of_existence","target")]
+startups_subset <- startups_subset[complete.cases(startups_subset),]
+
+
+DUMMY <- startups_subset$company_category_code
+DUMMY <- model.matrix(~DUMMY)
+startups_subset <- data.frame(startups_subset, DUMMY)
+rm(DUMMY)
+
+DUMMY <- startups_subset$funding_round_type
+DUMMY <- model.matrix(~DUMMY)
+startups_subset <- data.frame(startups_subset, DUMMY)
+rm(DUMMY)
+
+
+startups_subset <- startups_subset[which(startups_subset$days_of_existence<censoring_date),]
+
+#for regression
+X <- as.matrix(startups_subset[,-which(names(startups_subset) %in% c("company_category_code","target","X.Intercept.", "X.Intercept..1"))])
+Y <- as.matrix(startups_subset[,c("target")])
+
+
+#____________linear model_____________#
